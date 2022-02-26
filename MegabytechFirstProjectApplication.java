e4da8e9baa841b9e3f703c9981c03c5fd4abd7f2
Alaparthi Rajabhargavi chowdary <100462435+rajabhargavichowdary@users.noreply.github.com>
2022-02-26 13:02:31 +0530

    spring boot main method class file app context

---
+++ MegabytechFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.megabytech.first.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegabytechFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegabytechFirstProjectApplication.class, args);
+	}
+
+}
